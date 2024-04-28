30716d0099faa690ec239069de7a33ed1e30c34a
andrew <s83060207@yahoo.com.tw>
2024-04-28 21:11:55 +0800

    Update GitHub Pages config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack');
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/python_django_vue/frontenddisdist/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/python_django_vue/frontenddis/dist/' : '/',
   transpileDependencies: true,
   // assetsDir: 'static',
   configureWebpack: {
